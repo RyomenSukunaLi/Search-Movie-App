625be9cdd117d1ec1e222696c3a6cb5e6a32a123
RyumenSukuna14 <awaisazeemsahi786@gmail.com>
2025-12-03 03:28:52 +0500

    Build for GitHub Pages with correct base path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://ryomensukunali.github.io/Search-Movie-App/',
+  base: '/Search-Movie-App/',
 })
