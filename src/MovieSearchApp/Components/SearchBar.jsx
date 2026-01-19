87fa7a2f3d39b195e76ec896decba607709b310c
RyumenSukuna14 <awaisazeemsahi786@gmail.com>
2026-01-20 03:03:41 +0500

    Refactor: inline search input change handler in SearchBar

--- src/MovieSearchApp/Components/SearchBar.jsx
+++ src/MovieSearchApp/Components/SearchBar.jsx
@@ -5,9 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function SearchBar(){
     const navigate = useNavigate()
     const [searchValue, setSearchValue] = useState('');
-    const movieSearch = (e) => {
-       setSearchValue(e.target.value);
-    }
+
     const handleInput = (e) => {
         if(e.key == 'Enter' && searchValue.trim() !== ''){
             navigate(`/${searchValue}/1`);
@@ -16,7 +14,7 @@ function SearchBar(){
     return(<>
     <div className="flex items-center gap-4 border-2 border-[rgb(55,65,81)] bg-[rgb(31,41,55)] px-3 rounded-full w-1/2">
         <button><Search size={20} color='rgb(204,204,204)'/></button>
-        <input type="search" value={searchValue} className="border-none outline-none w-full py-4 text-[rgb(204,204,204)]" placeholder='Search for movies...' onChange={movieSearch} onKeyDown={handleInput}/>
+        <input type="search" value={searchValue} className="border-none outline-none w-full py-4 text-[rgb(204,204,204)]" placeholder='Search for movies...' onChange={(e) => {setSearchValue(e.target.value)}} onKeyDown={handleInput}/>
     </div>
     </>)
 }
